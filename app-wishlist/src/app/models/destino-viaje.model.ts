2af00a424a2b971d609a5e0de67194156b821ef7
Christian Mena <christian.91.gm@hotmail.com>
2019-06-06 22:07:51 -0500

    trabajando en prototipar y personalizar vistas

--- app-wishlist/src/app/models/destino-viaje.model.ts
+++ app-wishlist/src/app/models/destino-viaje.model.ts
@@ -3,13 +3,17 @@ export class DestinoViajeModel {
 
     private selected: boolean;
 
-    constructor(public nombre: string, public imagenUrl: string, public descripcion: string) { }
+    public servicios: string[];
+
+    constructor(public nombre: string, public imagenUrl: string, public descripcion: string) {
+        this.servicios = ['pileta','desayuno']
+     }
 
     isSelected(): boolean {
         return this.selected;
     }
 
-    setSelected(s:boolean) {
+    setSelected(s: boolean) {
         this.selected = s;
     }
 }
