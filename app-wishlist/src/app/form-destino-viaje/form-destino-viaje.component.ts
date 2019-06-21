09e048ee2d446d383a66fa8dd9470268f51bfa25
Christian Mena <christian.91.gm@hotmail.com>
2019-06-20 22:18:44 -0500

    Arquitectura de aplicaciones reactivas

--- app-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ app-wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinoViajeModel } from '../models/destino-viaje.model';
 import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
+import { fromEvent } from 'rxjs';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -12,6 +15,7 @@ export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViajeModel>;
   fg: FormGroup;
   miLongitud = 3;
+  searchResults: string[];
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
@@ -35,6 +39,17 @@ export class FormDestinoViajeComponent implements OnInit {
   }
 
   ngOnInit() {
+    let elemNombre = <HTMLInputElement>document.getElementById('nombre');
+    fromEvent(elemNombre, 'input')
+      .pipe(
+        map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+        filter(text => text.length > 2),
+        debounceTime(200),
+        distinctUntilChanged(),
+        switchMap(() => ajax('/assets/datos.json'))
+      ).subscribe(ajaxResponse => {
+        this.searchResults = ajaxResponse.response;
+      });
   }
 
   guardar(nombre: string, url: string, descripcion: string): boolean {
